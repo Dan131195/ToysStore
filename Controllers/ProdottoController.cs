3654dba552aa6bd2c4c237979954c4aee25a14d0
Dan131195 <daniele.franceschini1395@gmail.com>
2025-08-01 17:02:47 +0200

    Creazione ProdottoController + ProdottoService

---
+++ Controllers/ProdottoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToysStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdottoController : ControllerBase
+    {
+    }
+}
---
+++ Services/ProdottoService.cs
@@ -0,0 +1,17 @@
+﻿using ToysStore.Data;
+
+namespace ToysStore.Services
+{
+    public class ProdottoService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<ProdottoService> _logger;
+
+        public ProdottoService(ApplicationDbContext context, ILogger<ProdottoService> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+    }
+}
